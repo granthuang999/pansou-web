0b3bb52b4a95df2eb9637d4ce47070041c5ffb35
granthuang999 <66244223+granthuang999@users.noreply.github.com>
2025-08-31 15:32:03 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -349,7 +349,7 @@ onUnmounted(() => {
             </svg>
           </div>
           <div>
-            <h1 class="text-xl font-bold">PanSou</h1>
+            <h1 class="text-xl font-bold">盘搜-未来传媒</h1>
           </div>
         </div>
         
@@ -366,8 +366,11 @@ onUnmounted(() => {
             @click="switchToDocs"
             class="nav-button"
           >
-            <span class="nav-icon">📖</span>
-            API文档
+            <a href="https://pansou.futuremedia.work/" title="首页" style="text-decoration: none; color: inherit;">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" style="vertical-align: middle;">
+        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8h5z"></path>
+    </svg>
+</a>
           </button>
         </nav>
       </div>
@@ -434,9 +437,13 @@ onUnmounted(() => {
     <footer class="border-t border-border bg-background/50 backdrop-blur-sm mt-auto">
       <div class="container mx-auto px-4 py-4">
         <div class="flex items-center justify-center gap-4 text-sm text-muted-foreground">
+          <a href="https://www.futuremedia.work/about.html">关于本站</a> |
+<a href="https://www.futuremedia.work/terms-of-service.html">服务条款</a> |  
+<a href="https://www.futuremedia.work/privacy-policy.html">隐私政策</a> |
+<a href="https://www.futuremedia.work/disclaimer.html">免责声明</a> <br>
           <span>© {{ new Date().getFullYear() }}-{{ new Date().getFullYear() + 10 }}</span>
-          <a href="https://dm.xueximeng.com/" target="_blank" rel="noopener noreferrer" class="hover:text-foreground transition-colors">美漫资源共建</a>
-          <a href="https://github.com/fish2018" target="_blank" rel="noopener noreferrer" class="hover:text-foreground transition-colors">
+          <a href="https://www.futuremedia.work/" target="_blank" rel="noopener noreferrer" class="hover:text-foreground transition-colors">未来传媒</a>
+          <a href="https://github.com/granthuang999" target="_blank" rel="noopener noreferrer" class="hover:text-foreground transition-colors">
             <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 16 16">
               <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
             </svg>
